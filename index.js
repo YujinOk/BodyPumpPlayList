8f7aeecabb0b50a3051a8724472cb46147e93e60
YujinOk <yujinok63@gmail.com>
2022-03-20 14:46:03 +1100

    Added json file

---
+++ index.js
@@ -0,0 +1,8 @@
+import { search, lookup } from "https://cdn.skypack.dev/itunes-store-api";
+import { search } from "itunes-store-api";
+const { results } = await search("M83");
+// 1. randomise the playlist
+//  from different release no. change equivelent tracks alike
+// 2. be able to record what has played in that particular class
+
+// end point to trigger the function
